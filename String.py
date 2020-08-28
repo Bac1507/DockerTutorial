e0f7b6e4fcda871227746240afd5c900a1aaeab1
Bac1507 <nguyenthiback4@gmail.com>
2020-08-28 14:41:50 +0700

    String introduce with print

--- String.py
+++ String.py
@@ -1,4 +1,4 @@
-my_fullname = 'Nguyen Thi Bac'
+my_fullname = "Nguyen Thi Bac"
 my_nickname = 'Mon'
 my_age = 22
 my_height = 159
@@ -6,4 +6,15 @@ my_weight = 43
 my_eyes_color = 'black'
 my_hair_color = 'black'
 my_job = 'Telecomunication and Electronic'
-my_interests = 'movie, music, read comic and novel and shopping :v'
+my_interests = 'movie, music, read comic and novel, shopping :v'
+#
+#
+print("I'm " + my_fullname)
+print("You can call me is " + my_nickname)
+print("I'm", + my_age ," years ord ")
+print("I'm", + my_height , "centimets tall")
+print("I'm", + my_weight , "kilogram heavy")
+print("I like %s" % my_interests)
+print("I have %s" % my_eyes_color, "eyes")
+print("And I have %s" % my_hair_color," hair")
+print("Thanks for watching:v")
